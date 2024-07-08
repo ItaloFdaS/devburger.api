b18b67304827bc0540f7190f1ad027f04a21d3cf
ItaloFdaS <italofelipe26@hotmail.com>
2024-07-08 09:12:44 -0300

    refazendo as dependencias

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
-import app from "./app";
+import app from "./app.js";
 
-app.listen(3001, () => console.log("Server is runnig at port 3001..."));
+// TypeError: Cannot read properties of undefined (reading 'listen')
+
+app.listen(3001, () => console.log("server is runing at port 3001 ..."));
