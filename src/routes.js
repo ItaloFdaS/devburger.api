9049af440584a655fa01cce6bca494318a9ba659
ItaloFdaS <italofelipe26@hotmail.com>
2024-07-08 15:21:00 -0300

    ultimas att

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
 
+import { v4 } from "uuid";
+
+import User from "./app/models/User.js";
+
 const routes = new Router();
 
-routes.get("/", (resquest, response) => {
-  return response.status(200).json({ message: "Galo DOido" });
+routes.get("/", async (resquest, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: "italofelipe",
+    email: "italofelipe@email.com",
+    password_hash: "GaloDoido",
+  });
+
+  return response.status(201).json({ user });
 });
 
 export default routes;
