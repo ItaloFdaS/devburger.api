34a048d43647fa4402a0eebe6806ac12017fbe70
ItaloFdaS <italofelipe26@hotmail.com>
2024-07-09 10:47:29 -0300

    att aula yup

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,9 @@
 import { Router } from "express";
 
-import { v4 } from "uuid";
-
-import User from "./app/models/User.js";
+import UserController from "./app/controllers/UserController.js";
 
 const routes = new Router();
 
-routes.get("/", async (resquest, response) => {
-  const user = await User.create({
-    id: v4(),
-    name: "italofelipe",
-    email: "italofelipe@email.com",
-    password_hash: "GaloDoido",
-  });
-
-  return response.status(201).json({ user });
-});
+routes.post("/users", UserController.store);
 
 export default routes;
