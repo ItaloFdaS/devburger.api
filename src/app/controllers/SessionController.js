f9e0cde06035de8fea5e3e737a6a3f3c32092dd6
ItaloFdaS <italofelipe26@hotmail.com>
2024-07-10 18:30:30 -0300

    começando aula multer

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,48 @@
+import * as Yup from "yup";
+import User from "../models/User";
+
+class SessionController {
+  async store(request, response) {
+    const schema = Yup.object({
+      email: Yup.string().email().required(),
+      password: Yup.string().min(6).required(),
+    });
+
+    const isValid = await schema.isValid(request.body);
+
+    const emailOrPasswordIncorret = () =>
+      response
+        .status(401)
+        .json({ error: "make sure your email or password are correct" });
+    if (!isValid) {
+      return emailOrPasswordIncorret();
+    }
+
+    const { email, password } = request.body;
+
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return emailOrPasswordIncorret();
+    }
+
+    const isSamePassword = await user.checkPassword(password);
+
+    if (!isSamePassword) {
+      return emailOrPasswordIncorret();
+    }
+
+    return response.status(201).json({
+      id: user.id,
+      name: user.name,
+      email,
+      admin: user.admin,
+    });
+  }
+}
+
+export default new SessionController();
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -17,7 +17,7 @@ class UserController {
     const schema = Yup.object({
       name: Yup.string().required(),
       email: Yup.string().email().required(),
-      password_hash: Yup.string().min(6).required(),
+      password: Yup.string().min(6).required(),
       admin: Yup.boolean(),
     });
 
@@ -27,13 +27,25 @@ class UserController {
       return response.status(400).json({ error: err.errors });
     }
 
-    const { name, email, password_hash, admin } = request.body;
+    const { name, email, password, admin } = request.body;
+
+    const emailExists = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (emailExists) {
+      return response.status(400).json({
+        error: "Email already exists",
+      });
+    }
 
     const user = await User.create({
       id: v4(),
       name,
       email,
-      password_hash,
+      password,
       admin,
     });
 
