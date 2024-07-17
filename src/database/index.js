8a7166fbe669956c629f330ab71d1024cc14dc8d
ItaloFdaS <italofelipe26@hotmail.com>
2024-07-17 17:00:10 -0300

    #

--- src/database/index.js
+++ src/database/index.js
@@ -25,7 +25,7 @@ class Database {
 
   mongo() {
     this.mongoConnection = mongoose.connect(
-      "mongodb://localhost:27017/devburger-postgres"
+      "mongodb://localhost:27017/devburger"
     );
   }
 }
